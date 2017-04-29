535b433dfc53e4cf62f5503844beb06be0986420
turisap <kirill-shakirov@mail.ru>
2017-04-29 12:55:05 +0700

    Config file with info database connection added

--- Core/Model.php
+++ Core/Model.php
@@ -3,7 +3,7 @@
 namespace Core;
 
 use PDO;
-
+use App\Config;
 /**
  * Base model
  *
@@ -22,15 +22,12 @@ abstract class Model
         static $db = null;
 
         if ($db === null) {
-            $host = 'localhost';
-            $dbname = 'mvc';
-            $username = 'root';
-            $password = '';
+
     
             try {
-                $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", 
-                              $username, $password);
-
+                $dsn = 'mysql:host=' . Config::DB_HOST . ';dbname=' .
+                    Config::DB_NAME . ';charset=utf8';
+                $db = new PDO($dsn, Config::DB_USER, Config::DB_PASSWORD);
             } catch (PDOException $e) {
                 echo $e->getMessage();
             }
