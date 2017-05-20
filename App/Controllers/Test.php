dd6289f8e75242d572cea6dde6c4508111f038db
turisap <kirill-shakirov@mail.ru>
2017-05-20 13:05:38 +0700

    Got function rearry() (for multiply files upload working)

--- App/Controllers/Test.php
+++ App/Controllers/Test.php
@@ -19,21 +19,35 @@ class Test
 
 
     public static function test(){
-      print_r($_FILES);
+
+        print_r(self::reArrayFiles($_FILES['photos']));
+
+        //print_r($_FILES);
+
+
+        /*$file_count = count($_FILES['photos']);
+        $file_keys = array_keys($_FILES['photos']);
+        print_r($file_count);
+        echo '<br>';
+        print_r($file_keys);*/
 
     }
 
-    protected  static function reArrayFiles($file_post) {
+
+
+
+    protected static function reArrayFiles(&$file_post) {
 
         $file_ary = array();
-        $file_count = count($file_post['photos']);
+        $file_count = count($file_post['name']);
         $file_keys = array_keys($file_post);
 
-        for ($i=0; $i<$file_count; $i++) {
-            foreach ($file_keys as $key) {
+        for ($i=0; $i < $file_count; $i++){
+            foreach ($file_keys as $key){
                 $file_ary[$i][$key] = $file_post[$key][$i];
             }
         }
+
 
         return $file_ary;
     }
