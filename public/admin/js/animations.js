f2fccd964784f0421767f82dcec85a24fb906c48
turisap <kirill-shakirov@mail.ru>
2017-05-20 10:03:43 +0700

    fixed not working animations

--- public/admin/js/animations.js
+++ public/admin/js/animations.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
-    // time out for fading flash messages
+
+
     setTimeout(fadeMessage, 3000);
+
+
 });
 
 // fading out of flash messages
 function fadeMessage() {
     $('.flash_message').fadeOut(500);
 }
+
