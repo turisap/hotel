3801df82981cf13fd05e0c308b16d11f259baab0
turisap <kirill-shakirov@mail.ru>
2017-06-09 21:08:59 +0700

    got removing of a notification from navbar on click and setting read status to 0 through ajax

--- public/admin/js/notification-read.js
+++ public/admin/js/notification-read.js
@@ -6,21 +6,33 @@ $(document).ready(function () {
     $('.set-read').click(function () {
 
         // get notification id from link id attribute
-        var notification_id = $(this). attr('id');
+        var notification_id = $(this);
+        var idValue = notification_id.attr('id');
 
 
         // set picture as the main one
         $.ajax({
             url: '/admin/notifications/set-as-read',
             data: {
-                notification_id : notification_id
+                notification_id : idValue
             },
             type: 'POST',
             success: function(data){
 
                 if(!data.error){
-                    alert(notification_id);
-                    location.reload(true);
+
+                    // get value of notification counter and set less by one
+                    var counter = $('#notificationCount');
+                    var value = parseInt(counter.text());
+                    counter.text(Math.abs(value - 1));
+
+
+                    // remove notification from the dropdown
+                    var notification = notification_id.parents('.notification-container');
+                    //alert(JSON.stringify(notification, null, 4));
+                    notification.remove();
+                    
+
                 }
 
             }
