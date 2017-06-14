5cedab42523b393bb1ea2c3c09f2e06948e4bc1b
turisap <kirill-shakirov@mail.ru>
2017-06-14 19:47:10 +0700

    Got rid of three grapghs js

--- public/admin/js/google/google-chart.js
+++ public/admin/js/google/google-chart.js
@@ -20,6 +20,7 @@ gapi.analytics.ready(function() {
     });
 
 
+
     /**
      * Create a new ActiveUsers instance to be rendered inside of an
      * element with the id "active-users-container" and poll for changes every
@@ -75,9 +76,7 @@ gapi.analytics.ready(function() {
 
         // Render all the of charts for this view.
         renderWeekOverWeekChart(data.ids);
-        renderYearOverYearChart(data.ids);
-        renderTopBrowsersChart(data.ids);
-        renderTopCountriesChart(data.ids);
+
     });
 
 
@@ -152,127 +151,7 @@ gapi.analytics.ready(function() {
      * overlays session data for the current year over session data for the
      * previous year, grouped by month.
      */
-    function renderYearOverYearChart(ids) {
 
-        // Adjust `now` to experiment with different days, for testing only...
-        var now = moment(); // .subtract(3, 'day');
-
-        var thisYear = query({
-            'ids': ids,
-            'dimensions': 'ga:month,ga:nthMonth',
-            'metrics': 'ga:users',
-            'start-date': moment(now).date(1).month(0).format('YYYY-MM-DD'),
-            'end-date': moment(now).format('YYYY-MM-DD')
-        });
-
-        var lastYear = query({
-            'ids': ids,
-            'dimensions': 'ga:month,ga:nthMonth',
-            'metrics': 'ga:users',
-            'start-date': moment(now).subtract(1, 'year').date(1).month(0)
-                .format('YYYY-MM-DD'),
-            'end-date': moment(now).date(1).month(0).subtract(1, 'day')
-                .format('YYYY-MM-DD')
-        });
-
-        Promise.all([thisYear, lastYear]).then(function(results) {
-            var data1 = results[0].rows.map(function(row) { return +row[2]; });
-            var data2 = results[1].rows.map(function(row) { return +row[2]; });
-            var labels = ['Jan','Feb','Mar','Apr','May','Jun',
-                'Jul','Aug','Sep','Oct','Nov','Dec'];
-
-            // Ensure the data arrays are at least as long as the labels array.
-            // Chart.js bar charts don't (yet) accept sparse datasets.
-            for (var i = 0, len = labels.length; i < len; i++) {
-                if (data1[i] === undefined) data1[i] = null;
-                if (data2[i] === undefined) data2[i] = null;
-            }
-
-            var data = {
-                labels : labels,
-                datasets : [
-                    {
-                        label: 'Last Year',
-                        fillColor : 'rgba(220,220,220,0.5)',
-                        strokeColor : 'rgba(220,220,220,1)',
-                        data : data2
-                    },
-                    {
-                        label: 'This Year',
-                        fillColor : 'rgba(151,187,205,0.5)',
-                        strokeColor : 'rgba(151,187,205,1)',
-                        data : data1
-                    }
-                ]
-            };
-
-            new Chart(makeCanvas('chart-2-container')).Bar(data);
-            generateLegend('legend-2-container', data.datasets);
-        })
-            .catch(function(err) {
-                console.error(err.stack);
-            });
-    }
-
-
-    /**
-     * Draw the a chart.js doughnut chart with data from the specified view that
-     * show the top 5 browsers over the past seven days.
-     */
-    function renderTopBrowsersChart(ids) {
-
-        query({
-            'ids': ids,
-            'dimensions': 'ga:browser',
-            'metrics': 'ga:pageviews',
-            'sort': '-ga:pageviews',
-            'max-results': 5
-        })
-            .then(function(response) {
-
-                var data = [];
-                var colors = ['#4D5360','#949FB1','#D4CCC5','#E2EAE9','#F7464A'];
-
-                response.rows.forEach(function(row, i) {
-                    data.push({ value: +row[1], color: colors[i], label: row[0] });
-                });
-
-                new Chart(makeCanvas('chart-3-container')).Doughnut(data);
-                generateLegend('legend-3-container', data);
-            });
-    }
-
-
-    /**
-     * Draw the a chart.js doughnut chart with data from the specified view that
-     * compares sessions from mobile, desktop, and tablet over the past seven
-     * days.
-     */
-    function renderTopCountriesChart(ids) {
-        query({
-            'ids': ids,
-            'dimensions': 'ga:country',
-            'metrics': 'ga:sessions',
-            'sort': '-ga:sessions',
-            'max-results': 5
-        })
-            .then(function(response) {
-
-                var data = [];
-                var colors = ['#4D5360','#949FB1','#D4CCC5','#E2EAE9','#F7464A'];
-
-                response.rows.forEach(function(row, i) {
-                    data.push({
-                        label: row[0],
-                        value: +row[1],
-                        color: colors[i]
-                    });
-                });
-
-                new Chart(makeCanvas('chart-4-container')).Doughnut(data);
-                generateLegend('legend-4-container', data);
-            });
-    }
 
 
     /**
@@ -345,5 +224,7 @@ gapi.analytics.ready(function() {
         div.appendChild(document.createTextNode(str));
         return div.innerHTML;
     }
+
+
 
 });
